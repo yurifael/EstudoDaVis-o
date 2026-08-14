411960f3cf025086933899c5a6e08939963d5c19
yurifael <yuri.freitas.rosa@escola.pr.gov.br>
2026-08-14 08:47:55 -0300

    ok

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Botões de acessiblidade
+ //Botões de acessiblidade
 document.addventListener("DOMContentLoaded, () =>"){
 
 
@@ -21,8 +21,20 @@ document.body.classList.toogle("auto-contraste");
 const ativo = document.body.classList.contains("auto-contraste");
 btnContraste.setAtribute("aria-pressed", ativo);
 
-)
+)}")
 
+//função aumentar o texto
+btnAumentar.addEventListener("click", )=>{
+    if (tamanhoAtualFonte < 150){
+        tamanhoAtualFonte += 10;
+        document.documentElement.style.fontSize = `${tamanhoAtualFonte}%`;
+    }
+});
+//função diminuir texto;
 
-
-}")
+btnDiminuir.addEventListener("click", ())=>{
+    if(tamanhoAtualFonte > 90){
+        tamanhoAtualFonte-= 10;
+        document.documentElement.style.fontSize = `${tamanhoAtualFonte}%`;
+    }
+})
